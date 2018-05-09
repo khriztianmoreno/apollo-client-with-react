bbb813458ddfd525348992e34c7741722bfae716
khriztianmoreno <khriztianmoreno@gmail.com>
2018-05-09 16:10:45 -0500

    Code base

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          {
+            this.props.data.loading === true
+              ? "Loading"
+              : this.props.data.search.edges.map(data =>
+                <ul key={data.node.id}>
+                  <li style={{fontWeight: 'bold'}}>
+                    <a href={data.node.url}>{data.node.name}</a>
+                  </li>
+                  <li>{data.node.description}</li>
+                </ul>
+              )
+          }
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+const repoQuery = gql`
+  query($name: String!){
+    search(query: $name, last: 10, type: REPOSITORY) {
+      edges {
+        node {
+          ... on Repository {
+            id
+            name
+            description
+            url
+          }
+        }
+      }
+    }
+  }
+`
+
+const AppWithData = graphql(
+  repoQuery,
+  {
+    options: {
+      variables: {
+        name: "medellinjs"
+      }
+    }
+  }
+)(App)
+
+export default AppWithData;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+
+import './index.css';
+
+
+const token = 'YOUR_TOKEN';
+
+const httpLink = {
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${token}`
+  }
+};
+
+const client = new ApolloClient({
+  link: new HttpLink(httpLink),
+  cache: new InMemoryCache()
+});
+
+
+const Root = () => (
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
